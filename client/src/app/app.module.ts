8939c32ed2664be2bd6b6c774dda022a95cbd327
Rosanne Larocque <rosanne_larocque@hotmail.com>
2019-01-18 10:54:55 -0500

    conflict resolution with master AR RL

--- client/src/app/app.module.ts
+++ client/src/app/app.module.ts
@@ -5,13 +5,10 @@ import { AppComponent } from "./app.component";
 import { BasicService } from "./basic.service";
 import { HttpClientModule } from "@angular/common/http";
 import { InitialComponent } from './initial/initial.component';
-<<<<<<< HEAD
 import { AdminMenuComponent } from './adminView/admin-menu/admin-menu.component';
 import { FreeGeneratorComponent } from './adminView/free-generator/free-generator.component';
 import { SimpleGeneratorComponent } from './adminView/simple-generator/simple-generator.component';
-=======
 import { AppRoutingModule } from './/app-routing.module';
->>>>>>> master
 
 @NgModule({
   declarations: [
