ce59a5e75db610bbd396a9573de10de819c1e1ce
Rosanne Larocque <rosanne_larocque@hotmail.com>
2019-01-18 10:41:42 -0500

    RL AR empty routing module

--- client/src/app/app.module.ts
+++ client/src/app/app.module.ts
@@ -5,6 +5,7 @@ import { AppComponent } from "./app.component";
 import { BasicService } from "./basic.service";
 import { HttpClientModule } from "@angular/common/http";
 import { InitialComponent } from './initial/initial.component';
+import { AppRoutingModule } from './/app-routing.module';
 
 @NgModule({
   declarations: [
@@ -13,7 +14,8 @@ import { InitialComponent } from './initial/initial.component';
   ],
   imports: [
     BrowserModule,
-    HttpClientModule
+    HttpClientModule,
+    AppRoutingModule
   ],
   providers: [BasicService],
   bootstrap: [AppComponent]
