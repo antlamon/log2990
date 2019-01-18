fe16ef5dfad3766c51c646e2a67dd3d64a813918
Rosanne Larocque <rosanne_larocque@hotmail.com>
2019-01-18 11:26:03 -0500

    rooting pursued AR RL

--- client/src/app/app-routing.module.ts
+++ client/src/app/app-routing.module.ts
@@ -2,9 +2,17 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterModule, Routes } from '@angular/router';
 
+import { AdminMenuComponent } from './adminView/admin-menu/admin-menu.component';
+import { FreeGeneratorComponent } from './adminView/free-generator/free-generator.component';
+import { SimpleGeneratorComponent} from './adminView/simple-generator/simple-generator.component';
+
+const routes: Routes = [
+  { path: 'admin', component: AdminMenuComponent},
+];
 
 
 @NgModule({
+  exports: [ RouterModule ],
   imports: [
     CommonModule
   ],
