963cebf3cece6470969047dabebfc4d9f0a5c375
Rosanne Larocque <rosanne_larocque@hotmail.com>
2019-01-18 12:36:23 -0500

    app routing correction RL

--- client/src/app/app-routing.module.ts
+++ client/src/app/app-routing.module.ts
@@ -5,9 +5,11 @@ import { RouterModule, Routes } from '@angular/router';
 import { AdminMenuComponent } from './adminView/admin-menu/admin-menu.component';
 import { FreeGeneratorComponent } from './adminView/free-generator/free-generator.component';
 import { SimpleGeneratorComponent} from './adminView/simple-generator/simple-generator.component';
+import { InitialComponent} from './initial/initial.component';
 
 const routes: Routes = [
   { path: 'admin', component: AdminMenuComponent},
+  { path: '', component: InitialComponent },
   { path: 'freegen', component: FreeGeneratorComponent},
   { path: 'simplegen', component: SimpleGeneratorComponent},
 ];
