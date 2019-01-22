6089d908c38f82a6ab16b35c939400b093e2342f
Rosanne Larocque <rosanne_larocque@hotmail.com>
2019-01-22 00:28:32 -0500

    added childRooting for forms RL

--- client/src/app/adminView/admin-menu/admin-menu.component.ts
+++ client/src/app/adminView/admin-menu/admin-menu.component.ts
@@ -1,15 +1,36 @@
 import { Component, OnInit } from '@angular/core';
+import { Router, ActivatedRoute} from '@angular/router';
 
 @Component({
   selector: 'app-admin-menu',
   templateUrl: './admin-menu.component.html',
-  styleUrls: ['./admin-menu.component.css']
+  styleUrls: ['./admin-menu.component.css'],
 })
 export class AdminMenuComponent implements OnInit {
 
-  constructor() { }
+  isHidden: boolean;
+
+  constructor(private router: Router, private route: ActivatedRoute) {
+    this.isHidden=false;
+  }
 
   ngOnInit() {
+  }
+
+  openFreeViewForm() {
+    this.router.navigate(['freegen'], {relativeTo: this.route});
+    this.isHidden=true;
+  }
+
+  
+
+  openSimpleViewForm() {
+    this.router.navigate(['simplegen'], {relativeTo: this.route});
+    this.isHidden=true;
+  }
+  closeSimpleViewForm() {
+    this.router.navigate([''], {relativeTo: this.route});
+    this.isHidden=false;
   }
 
 }
