a6a5523822a59e7a475a393cf9b61edb25fd5acd
Rosanne Larocque <rosanne_larocque@hotmail.com>
2019-01-22 00:27:37 -0500

    added chlidPath for admin menu RL

--- client/src/app/app-routing.module.ts
+++ client/src/app/app-routing.module.ts
@@ -1,5 +1,4 @@
 import { NgModule } from '@angular/core';
-import { CommonModule } from '@angular/common';
 import { RouterModule, Routes } from '@angular/router';
 
 import { AdminMenuComponent } from './adminView/admin-menu/admin-menu.component';
@@ -8,15 +7,19 @@ import { SimpleGeneratorComponent} from './adminView/simple-generator/simple-gen
 import { InitialComponent} from './initial/initial.component';
 
 const routes: Routes = [
-  { path: 'admin', component: AdminMenuComponent},
-  { path: '', component: InitialComponent },
-  { path: 'freegen', component: FreeGeneratorComponent},
-  { path: 'simplegen', component: SimpleGeneratorComponent},
+  { path: 'admin',
+   component: AdminMenuComponent,
+    children: [
+    { path: 'freegen', component: FreeGeneratorComponent},
+    { path: 'simplegen', component: SimpleGeneratorComponent}
+   ]
+  },
+  { path: '', component: InitialComponent }
 ];
 
 
 @NgModule({
-  imports:[ RouterModule.forRoot(routes) ],
+  imports:[ RouterModule.forRoot(routes) ], 
   exports: [ RouterModule ],  declarations: []
 })
 export class AppRoutingModule { }
