a255cfd142ef30d3b9415fbef30c8ba700a31f15
STEPHANE MICHAUD <stephane.michaud@polymtl.ca>
2019-01-23 12:21:30 -0500

    Ajout de commentaires

--- client/src/app/initial/initial.component.ts
+++ client/src/app/initial/initial.component.ts
@@ -1,62 +1,61 @@
-import {Component, OnInit} from '@angular/core';
-import{NAMES} from '../mock-names'
+import {Component, OnInit} from "@angular/core";
+import{NAMES} from "../mock-names";
 
 @Component({
-  selector: 'app-initial',
-  templateUrl: './initial.component.html',
-  styleUrls: ['./initial.component.css']
+  selector: "app-initial",
+  templateUrl: "./initial.component.html",
+  styleUrls: ["./initial.component.css"]
 })
 export class InitialComponent implements OnInit {
 
-  readonly MIN_LENGTH:number =3;
-  readonly MAX_LENGTH:number =10;
+  public readonly MIN_LENGTH: number = 3;
+  public readonly MAX_LENGTH: number = 10;
 
-  readonly  ERROR_ID:string='error';
-  readonly  ERROR_MESSAGE_INVALID_NAME= 'The name entered was invalid';
-  readonly  ERROR_MESSAGE_ALREADY_USED_NAME='The name is already being used';
+  public readonly  ERROR_ID: string = "error";
+  public readonly  ERROR_MESSAGE_INVALID_NAME: string = "The name entered was invalid";
+  public readonly  ERROR_MESSAGE_ALREADY_USED_NAME: string = "The name is already being used";
 
-  public names:string[];
+  public names: string[];
 
-  username:string;
+  public username: string;
 
-  constructor() {
-    this.username='';//invalid name
-    //Mock values for testing
-    this.names=NAMES;
+  public constructor() {
+    this.username = ""; // invalid name
+    // Mock values for testing
+    this.names = NAMES;
   }
 
-  ngOnInit() {
+  public ngOnInit() {
 
   }
-  public connect(username:string):void
-  {
-    if(!(this.isValidUsername(username))) {
+  public connect(username: string): void {
+    if (!(this.isValidUsername(username))) {
       this.showErrorMessage(this.ERROR_MESSAGE_INVALID_NAME);
       return;
     }
-    //To simulate a server database, must be changed
-    if(this.names.includes(username)) {
+    // To simulate a server database, must be changed
+    if (this.names.includes(username)) {
       this.showErrorMessage(this.ERROR_MESSAGE_ALREADY_USED_NAME);
       return;
     }
-    //Change view
+    // Change view
     this.names.push(username);
   }
-   public isValidUsername(nom:string):boolean
-  {
-    return nom.length<this.MAX_LENGTH && nom.length>this.MIN_LENGTH && this.containOnlyAlphaNumeric(nom);
+  public isValidUsername(nom: string): boolean {
+    return nom.length < this.MAX_LENGTH && nom.length > this.MIN_LENGTH && this.containOnlyAlphaNumeric(nom);
   }
-  public containOnlyAlphaNumeric(nom:string):boolean {
-    let check= nom.match(/^[a-zA-Z0-9]+$/i);
-    return check==null ? false : check[0].length==nom.length
+  public containOnlyAlphaNumeric(nom: string): boolean {
+    const check = nom.match(/^[a-zA-Z0-9]+$/i);
+
+    return check == null ? false : check[0].length === nom.length;
   }
 
-  showErrorMessage(message:string):void
-  {
-    let errorBox:HTMLElement = <HTMLElement>document.getElementById(this.ERROR_ID);
+  private showErrorMessage(message: string): void {
+    const errorBox: HTMLElement = document.getElementById(this.ERROR_ID) as HTMLElement;
 
-    errorBox.textContent=message;
-    errorBox.style.opacity='1';
+    // DEMANDER POUR JQUERY
+    errorBox.textContent = message;
+    errorBox.style.opacity = "1";
   }
 
 }
