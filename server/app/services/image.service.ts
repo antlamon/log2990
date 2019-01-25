f7513c423b4ed71338bfae2a03e31e6e4cf625be
Jonathan <jonathan.helias@polymtl.ca>
2019-01-25 12:30:27 -0500

    elargissement a complete

--- server/app/services/image.service.ts
+++ server/app/services/image.service.ts
@@ -13,8 +13,8 @@ export class ImageService {
 
     public getDifferentImage(req: Request, res: Response ) {
         
-        const path1: string = "./app/documents/image1.bmp";
-        const path2: string = "./app/documents/image2.bmp";
+        const path1: string = "./app/documents/gros1.bmp";
+        const path2: string = "./app/documents/gros2.bmp";
         const image1: ImageBMP= this.convertImage.bufferToImageBMP(readFileSync(path1));
         const image2: ImageBMP= this.convertImage.bufferToImageBMP(readFileSync(path2));
         console.log("imagedCompared");
@@ -32,9 +32,21 @@ export class ImageService {
         for(let i =0; i < image1.height; i++) {
             for(let j=0; j < image1.width; j++) {
 
+                //imageCompared.pixels[i][j] = {red: 255,green:255, blue: 255};
                 imageCompared.pixels[i][j] = this.comparePixel(image1.pixels[i][j],image2.pixels[i][j]);
-                if(imageCompared.pixels[i][j] != {red: 0, green:0, blue: 0})
-                    imageCompared.pixels[i][j] = {red: 0,green:255, blue: 255};
+
+                if(this.isBlackPixel(imageCompared.pixels[i][j])) 
+                    imageCompared.pixels[i][j] = {red: 255,green:255, blue: 255};
+                else
+                {
+                    for(let x:number = -3; x<= 3; x++) {
+                        for(let y:number =-3; y<= 3; y++) {
+
+                            if(Math.abs(x)+Math.abs(y) <= 4 && i+x >= 0 && i+x < image1.height && i+y >= 0 && i+y < image1.width)
+                                imageCompared.pixels[i+x][j+y] = {red: 0, green: 0, blue: 0};
+                        }
+                    }
+                }
             }
         }
         return imageCompared;
@@ -46,7 +58,11 @@ export class ImageService {
             red: pixel1.red - pixel2.red,
             green: pixel1.green - pixel2.green,
             blue: pixel1.blue - pixel2.blue
-        }
+        } as Pixel
+    }
+    isBlackPixel(pixel: Pixel): boolean {
+
+        return (pixel.blue === 0 && pixel.green === 0 && pixel.red === 0);
     }
 
 }
