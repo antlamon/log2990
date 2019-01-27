8c5dd165b396b7d06a063c9a1c3375a54d21da95
Annie <annie1.rochette@hotmail.com>
2019-01-27 17:16:14 -0500

    Correcting compilation errors - AR

--- client/src/app/adminView/simple-generator/simple-generator.component.ts
+++ client/src/app/adminView/simple-generator/simple-generator.component.ts
@@ -1,6 +1,14 @@
 import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
-import { FormGroup, FormBuilder, Validators } from '@angular/forms';
+// import { FormGroup, FormBuilder, Validators } from '@angular/forms';
+
+const FILE_FORMAT = "bmp";
+const IMAGE_WIDTH = 640;
+const IMAGE_HEIGHT = 480; 
+
+let correctModifiedFile = false;
+let correctOriginalFile = false;
+
 
 @Component({
   selector: 'app-simple-generator',
@@ -10,29 +18,61 @@ import { FormGroup, FormBuilder, Validators } from '@angular/forms';
 export class SimpleGeneratorComponent implements OnInit {
 
 
-  constructor(private router: Router, private fb: FormBuilder) { 
+  constructor(private router: Router) { 
+    
   }
 
-  ngOnInit() {
+  public ngOnInit(): void {
+    while(correctModifiedFile == true && correctOriginalFile == true){
+      if (document.getElementById("submitButton")) {
+        const boutonSub: HTMLButtonElement | null= document.getElementById("submitButton") as HTMLButtonElement;
+        boutonSub.disabled = false;
+      }
+    }
   }
 
-  onFileChange(event) {
+  public onFileChange(event: any): void {
+    
+  }
+
+  onModifiedFileChange(event: any){
     const reader = new FileReader();
 
-    if(event.target.files && event.target.files.length && this.checkExtension()) {
+    if(event.target.files && event.target.files.length && this.checkModifiedExtension()) {
       const [file] = event.target.files;
       reader.readAsArrayBuffer(file);
-      document.getElementById("submitButton").disabled = false;
       
       reader.onload = () => {
-        let buffer = reader.result;
+        let buffer: ArrayBuffer= reader.result as ArrayBuffer;
         let bmpWidth = new DataView(buffer); 
         let bmpHeight = new DataView(buffer); 
-        let width = bmpWidth.getUint32(18,true);
-        let height = bmpHeight.getUint32(22,true);
+
+        if(bmpWidth.getUint32(18,true) == IMAGE_WIDTH && bmpHeight.getUint32(22,true) == IMAGE_HEIGHT){
+          correctModifiedFile = true;
+        }
       };
     }
-    else{ document.getElementById("submitButton").disabled = true; }
+    else{ correctModifiedFile = false; }
+  }
+
+  onOriginalFileChange(event: any){
+    const reader = new FileReader();
+
+    if(event.target.files && event.target.files.length && this.checkOriginalExtension()) {
+      const [file] = event.target.files;
+      reader.readAsArrayBuffer(file);
+      
+      reader.onload = () => {
+        let buffer: ArrayBuffer = reader.result as ArrayBuffer;
+        let bmpWidth = new DataView(buffer); 
+        let bmpHeight = new DataView(buffer); 
+
+        if(bmpWidth.getUint32(18,true) == IMAGE_WIDTH && bmpHeight.getUint32(22,true) == IMAGE_HEIGHT){
+          correctOriginalFile = true;
+        }
+      };
+    }
+    else{ correctOriginalFile = false; }
   }
 
   submit() {
@@ -44,14 +84,21 @@ export class SimpleGeneratorComponent implements OnInit {
     this.router.navigate(['admin']); // go back to admin home
   }
  
-  checkExtension(): boolean {
+  checkOriginalExtension(){
    
-    let filename = document.getElementById("fileOrigin").value; 
-    let extension = filename.slice((filename.lastIndexOf(".") - 1 >>> 0) + 2);
+    let filenameOriginal = (document.getElementById("originalFile") as HTMLInputElement).value; 
+    let extensionOriginal = filenameOriginal.slice((filenameOriginal.lastIndexOf(".") - 1 >>> 0) + 2);
 
-    return (extension == "bmp")
+    return (extensionOriginal == FILE_FORMAT);
+
   }
 
+  checkModifiedExtension(){
+    let filenameModified = (document.getElementById("modifiedFile") as HTMLInputElement).value; 
+    let extensionModified = filenameModified.slice((filenameModified.lastIndexOf(".") - 1 >>> 0) + 2);
+
+    return (extensionModified == FILE_FORMAT);
+  }
 }
 
 
