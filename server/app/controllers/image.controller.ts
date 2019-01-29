00127b9d131d11f84bd3fb7d4c71917bd3b70dac
Jonathan <jonathan.helias@polymtl.ca>
2019-01-28 21:24:55 -0500

    commit des premiers tests

--- server/app/controllers/image.controller.ts
+++ server/app/controllers/image.controller.ts
@@ -1,23 +1,21 @@
+import { Request, Response, Router } from "express";
 import { inject, injectable  } from "inversify";
 import { ImageService } from "../services/image.service";
 import Types from "../types";
-import { Router, Request, Response, NextFunction } from "express";
 
 @injectable()
 export class ImageController {
-    
+
     public readonly url: string = "/imagegen";
-    
+
     public constructor(@inject(Types.ImageService) private imageService: ImageService) { }
 
     public get router(): Router {
 
         const router: Router = Router();
 
-        router.post("/",
-            (req: Request, res: Response) => {
-                this.imageService.getDifferentImage(req, res);
-                });
+        router.post("/", (req: Request, res: Response) => { this.imageService.getDifferentImage(req, res); });
+
         return router;
     }
 }
--- server/app/services/image.service.ts
+++ server/app/services/image.service.ts
@@ -1,77 +1,84 @@
-import { Message } from "../../../common/communication/message";
+import { Request, Response } from "express";
+import { readFileSync, writeFileSync } from "fs";
 import { inject, injectable } from "inversify";
 import "reflect-metadata";
-import { Pixel, ImageBMP, ConvertImage } from "./convertImage.service";
 import Types from "../types";
-import { Request, Response } from "express";
-import { readFile, readFileSync, writeFileSync } from "fs";
+import { ConvertImage, ImageBMP, Pixel,  } from "./convertImage.service";
 
 @injectable()
 export class ImageService {
 
     public constructor(@inject(Types.ConvertImage) private convertImage: ConvertImage) { }
 
-    public getDifferentImage(req: Request, res: Response) {
-        const path1: string = "./app/documents/gros1.bmp";
-        const path2: string = "./app/documents/gros2.bmp";
-        let bufferImage: Buffer = readFileSync(path1);
+    public getDifferentImage(req: Request, res: Response): void {
+        console.log("req.body.image1");
+        const path1: string = req.body.image1;
+        const path2: string = req.body.image2;
+        const bufferImage: Buffer = readFileSync(path1);
         const image1: ImageBMP = this.convertImage.bufferToImageBMP(bufferImage);
         const image2: ImageBMP = this.convertImage.bufferToImageBMP(readFileSync(path2));
-        console.log("imageCompared");
-        let imagedCompared: ImageBMP = this.compareData(image1, image2);
-        console.log("imageCompared");
-        this.convertImage.imageBMPtoBuffer(imagedCompared, bufferImage);
-        writeFileSync("./app/documents/result.bmp", bufferImage);
-        res.json(imagedCompared);
+
+        try {
+            const imagedCompared: ImageBMP = this.compareData(image1, image2);
+            this.convertImage.imageBMPtoBuffer(imagedCompared, bufferImage);
+            writeFileSync("./app/documents/result.bmp", bufferImage);
+
+        } catch (error) {
+            res.json(error);
+        }
     }
 
-    private compareData(image1: ImageBMP, image2: ImageBMP): ImageBMP {
-        let imageCompared: ImageBMP = image1;
-        let pixels: Pixel[][] = [];
-        let differentPixels: [number, number][] = [];
-
-        for (let i = 0; i < image1.height; i++) {
+    public compareData(image1: ImageBMP, image2: ImageBMP): ImageBMP {
+        const imageCompared: ImageBMP = image1;
+        const pixels: Pixel[][] = [];
+        const differentPixels: [number, number][] = [];
+        if (image1.height !== image2.height || image1.width !== image2.width) {
+            throw Error("La taille des deux images n'est pas la bonne");
+        }
+        for (let i: number = 0; i < image1.height; i++) {
             pixels[i] = [];
-            for (let j = 0; j < image1.width; j++) {
+            for (let j: number = 0; j < image1.width; j++) {
                 pixels[i][j] = this.comparePixel(image1.pixels[i][j], image2.pixels[i][j]);
 
-                if (this.isBlackPixel(pixels[i][j]))
+                if (this.isBlackPixel(pixels[i][j])) {
                     pixels[i][j] = { red: 255, green: 255, blue: 255 };
-                else {
+                } else {
                     pixels[i][j] = { red: 0, green: 0, blue: 0 };
-                    differentPixels.push([i,j]);
+                    differentPixels.push([i, j]);
                 }
             }
         }
         imageCompared.pixels = pixels;
         this.enlargeBlackPixels(imageCompared, differentPixels);
+
         return imageCompared;
     }
 
-    private enlargeBlackPixels(image: ImageBMP, pixelsToEnlarge: [number, number][]): void {
-        pixelsToEnlarge.forEach(element => {
+    public enlargeBlackPixels(image: ImageBMP, pixelsToEnlarge: [number, number][]): void {
+        pixelsToEnlarge.forEach((element: [number, number]) => {
             const i: number = element[0];
             const j: number = element[1];
             for (let y: number = -3; y <= 3; y++) {
                 if (i + y >= 0 && i + y < image.height) {
                     for (let x: number = -3; x <= 3; x++) {
-                        if (Math.abs(x) + Math.abs(y) <= 4 && i + x >= 0 && i + x < image.width)
+                        if (Math.abs(x) + Math.abs(y) <= 4 && i + x >= 0 && i + x < image.width) {
                             image.pixels[i + y][j + x] = { red: 0, green: 0, blue: 0 };
+                        }
                     }
                 }
             }
         });
     }
 
-    private comparePixel(pixel1: Pixel, pixel2: Pixel): Pixel {
+    public comparePixel(pixel1: Pixel, pixel2: Pixel): Pixel {
         return {
             red: pixel1.red - pixel2.red,
             green: pixel1.green - pixel2.green,
-            blue: pixel1.blue - pixel2.blue
-        } as Pixel
+            blue: pixel1.blue - pixel2.blue,
+        } as Pixel;
     }
 
-    private isBlackPixel(pixel: Pixel): boolean {
+    public isBlackPixel(pixel: Pixel): boolean {
         return (pixel.blue === 0 && pixel.green === 0 && pixel.red === 0);
     }
 
