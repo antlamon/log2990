6a0224f556be77fccdbcdb6bef0026c3e598fda0
antoine lamontagne <antoine.lamontagne@hotmail.ca>
2019-01-30 09:26:09 -0500

    fix routing

--- client/src/app/app-routing.module.ts
+++ client/src/app/app-routing.module.ts
@@ -1,24 +1,25 @@
 import { NgModule } from '@angular/core';
-import { RouterModule, Routes } from '@angular/router';
+import { RouterModule, Routes, Route } from '@angular/router';
 
 import { AdminMenuComponent } from './adminView/admin-menu/admin-menu.component';
 import { FreeGeneratorComponent } from './adminView/free-generator/free-generator.component';
-import { SimpleGeneratorComponent} from './adminView/simple-generator/simple-generator.component';
-import { InitialComponent} from './initial/initial.component';
+import { SimpleGeneratorComponent } from './adminView/simple-generator/simple-generator.component';
+import { InitialComponent } from './initial/initial.component';
 
 const routes: Routes = [
-  { path: 'admin',
+  {
+    path: 'admin',
     component: AdminMenuComponent,
     children: [
-    { path: 'freegen', component: FreeGeneratorComponent},
-    { path: 'simplegen', component: SimpleGeneratorComponent}
-   ]
+      { path: 'freegen', component: FreeGeneratorComponent } as Route,
+      { path: 'simplegen', component: SimpleGeneratorComponent } as Route,
+    ]
   },
   { path: '', component: InitialComponent }
 ];
 
 @NgModule({
-  imports: [ RouterModule.forRoot(routes) ],
-  exports: [ RouterModule ],  declarations: []
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule], declarations: []
 })
 export class AppRoutingModule { }
