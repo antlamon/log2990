8eaffb2941e7011d32932b3ed8d47c1118747781
Rosanne Larocque <rosanne_larocque@hotmail.com>
2019-01-29 19:57:48 -0500

    added mock Games

---
+++ client/src/app/model/mock-freeGames.ts
@@ -0,0 +1,7 @@
+import { Game } from './game';
+
+export const FreeGames: Game[] = [ new Game("Nissan Patrol", "../../../../images/1.bmp"),
+new Game("Jello", "../../../../images/1.bmp"), 
+new Game("Apple Pie", "../../../../images/1.bmp"),
+new Game("Lemon Pie", "../../../../images/1.bmp"),
+new Game("Cake Patrol", "../../../../images/1.bmp")];
