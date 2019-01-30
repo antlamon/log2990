096687f17398a3122ef4dccd17b1cef87aaf162b
STEPHANE MICHAUD <stephane.michaud@polymtl.ca>
2019-01-30 12:56:05 -0500

    {S.M} Fixed problems at compilation

--- client/src/app/app-routing.module.ts
+++ client/src/app/app-routing.module.ts
@@ -1,10 +1,9 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
-
-import { AdminMenuComponent } from './adminView/admin-menu/admin-menu.component';
-import { FreeGeneratorComponent } from './adminView/free-generator/free-generator.component';
-import { SimpleGeneratorComponent} from './adminView/simple-generator/simple-generator.component';
-import { InitialComponent} from './initial/initial.component';
+import {AdminMenuComponent} from "./adminView/admin-menu/admin-menu.component";
+import {FreeGeneratorComponent} from "./adminView/free-generator/free-generator.component";
+import {SimpleGeneratorComponent} from "./adminView/simple-generator/simple-generator.component";
+import {InitialComponent} from "./initial/initial.component";
 
 const routes: Routes = [
   { path: 'admin',
