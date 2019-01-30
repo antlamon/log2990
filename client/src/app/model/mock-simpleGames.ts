a795e69be01442e0005309c15a535f1e8bb76f7c
Rosanne Larocque <rosanne_larocque@hotmail.com>
2019-01-29 22:11:34 -0500

    no more mock class needed .. deleted

--- client/src/app/model/mock-simpleGames.ts
+++
@@ -1,7 +0,0 @@
-import { Game } from './game';
-
-export const SimpleGames: Game[] = [ new Game("Nissan Patrol", "1.jpg"),
-new Game("Jello", "../../../../images/1.jpg"), 
-new Game("Apple Pie", "../../../../images/1.bmp"),
-new Game("Lemon Pie", "../../../../images/1.bmp"),
-new Game("Cake Patrol", "../../../../images/1.bmp")];
