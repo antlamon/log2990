f4e147cfa3b1dea246137e3e69fe227cc18e5ed4
STEPHANE MICHAUD <stephane.michaud@polymtl.ca>
2019-01-30 12:57:51 -0500

    {S.M} removed null test in connexion.service

--- server/app/services/connexion.service.ts
+++ server/app/services/connexion.service.ts
@@ -1,7 +1,6 @@
 import { Message,ERROR_ID,BASE_ID } from "../../../common/communication/message";
 import "reflect-metadata";
 import { injectable } from "inversify";
-import {NAMES} from "../mock-names";
 import {UsersManagerInstance} from "./users.service";
 
 @injectable()
@@ -9,16 +8,12 @@ export class ConnexionService {
     public static readonly MIN_LENGTH: number = 3;
     public static readonly MAX_LENGTH: number = 10;
 
-    public names: string[];
 
     constructor(){
-        this.names=NAMES;
     }
 
     public async addName(newName:string,id:string): Promise<Message> {
 
-        if(newName ===null)
-            return {title:ERROR_ID,body:"Name is null"};
         if(!ConnexionService.isCorrectLength(newName))
             return {title:ERROR_ID,body:"Name is not the correct length it must be between "+ConnexionService.MIN_LENGTH+" and "+ConnexionService.MAX_LENGTH};
         if(!ConnexionService.containOnlyAlphaNumeric(newName))
@@ -32,15 +27,9 @@ export class ConnexionService {
         return {title:BASE_ID,body:"The name"+newName+" was added to the list of names"};
     }
     public static isCorrectLength(nom: string): boolean {
-        if(nom===null) {
-            return false;
-        }
         return nom.length <= this.MAX_LENGTH && nom.length >= this.MIN_LENGTH;
     }
     public static containOnlyAlphaNumeric(nom: string): boolean {
-        if(nom===null) {
-            return false;
-        }
         const check: RegExpMatchArray | null = nom.match(/^[a-zA-Z0-9]+$/i);
 
         return check === null ? false : check[0].length === nom.length;
