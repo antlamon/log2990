c9e5a718a737ca8620848e0541c2c657efe53040
Antoine Lamontagne <antoine.lamontagne@hotmail.ca>
2019-01-30 18:09:40 -0500

    fix dateservice test

--- server/app/services/date.service.spec.ts
+++ server/app/services/date.service.spec.ts
@@ -1,32 +1,35 @@
-import { expect } from "chai";
+import Axios from "axios";
+import * as chai from "chai";
+import * as spies from "chai-spies";
 import { Container } from "inversify";
-import { injectable } from "inversify";
 import "reflect-metadata";
 import { Message } from "../../../common/communication/message";
 import { TYPES } from "../types";
 import { DateService } from "./date.service";
-
-@injectable()
-class DateServiceBidon {
-
-    public currentTime(): Promise<Message> {
-        return new Promise<Message>(function(resolve, reject) {
-                                        resolve({title: "Time",
-                                                 body: "Aujourd'hui"});
-                                    });
-    }
-}
+import { IDate } from "./IDate";
 
 const container: Container = new Container();
-container.bind<DateService>(TYPES.DateService).to(DateServiceBidon);
+container.bind<DateService>(TYPES.DateService).to(DateService);
 
 const service: DateService =
      container.get<DateService>(TYPES.DateService);
 
+chai.use(spies);
+const expect: Chai.ExpectStatic = chai.expect;
+
 describe("function currentTime", () => {
     it("should return correct date", async () => {
+        const mockedData: IDate = {
+            currentDateTime: "24:00",
+            timeZoneName: "",
+            dayOfTheWeek: "",
+        };
+        chai.spy.on(Axios, "get", () => Promise.resolve({
+            data: mockedData,
+        }));
         const result: Message = await service.currentTime();
-        expect(result.body).to.equal("Aujourd'hui");
+        expect(result.title).to.equal("Time");
+        expect(result.body).to.equal(mockedData.currentDateTime);
     });
 
 });
