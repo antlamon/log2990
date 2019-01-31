fdf98b9a1f5bf326178698f9b690144c1babff08
Annie <annie1.rochette@hotmail.com>
2019-01-31 14:44:33 -0500

    Add route import - AR

--- client/src/app/app-routing.module.ts
+++ client/src/app/app-routing.module.ts
@@ -1,5 +1,5 @@
 import { NgModule } from '@angular/core';
-import { RouterModule, Routes } from '@angular/router';
+import { RouterModule, Routes, Route } from '@angular/router';
 import { AdminMenuComponent } from './adminView/admin-menu/admin-menu.component';
 import { FreeGeneratorComponent } from './adminView/free-generator/free-generator.component';
 import { SimpleGeneratorComponent} from './adminView/simple-generator/simple-generator.component';
