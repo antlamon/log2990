5aae2a98456721426f74328862c578d31a70004a
Antoine Lamontagne <antoine.lamontagne@hotmail.ca>
2019-01-30 19:30:51 -0500

    tests for date service and index service

--- server/app/services/date.service.spec.ts
+++ server/app/services/date.service.spec.ts
@@ -1,30 +1,34 @@
 import Axios from "axios";
 import * as chai from "chai";
 import * as spies from "chai-spies";
-import { Container } from "inversify";
 import "reflect-metadata";
 import { Message } from "../../../common/communication/message";
-import { TYPES } from "../types";
 import { IDate } from "./IDate";
 import { DateService } from "./date.service";
-
-const container: Container = new Container();
-container.bind<DateService>(TYPES.DateService).to(DateService);
-
-const service: DateService =
-     container.get<DateService>(TYPES.DateService);
 
 chai.use(spies);
 const expect: Chai.ExpectStatic = chai.expect;
 
 describe("function currentTime", () => {
+    let service: DateService;
+    let sandbox: ChaiSpies.Sandbox;
+
+    beforeEach(() => {
+        service = new DateService();
+        sandbox = chai.spy.sandbox();
+    });
+
+    afterEach(() => {
+        sandbox.restore();
+    });
+
     it("should return correct date", async () => {
         const mockedData: IDate = {
             currentDateTime: "24:00",
             timeZoneName: "",
             dayOfTheWeek: "",
         };
-        chai.spy.on(Axios, "get", () => Promise.resolve({
+        sandbox.on(Axios, "get", () => Promise.resolve({
             data: mockedData,
         }));
         const result: Message = await service.currentTime();
