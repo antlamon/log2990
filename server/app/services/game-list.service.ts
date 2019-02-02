10886be07aa280a9d4cafe8720543b5bddd05dbc
Antoine Lamontagne <antoine.lamontagne@hotmail.ca>
2019-02-02 15:07:12 -0500

    game-list service tslint

--- server/app/services/game-list.service.ts
+++ server/app/services/game-list.service.ts
@@ -1,32 +1,25 @@
 
+import { writeFileSync } from "fs";
 import { inject, injectable } from "inversify";
 import "reflect-metadata";
-
-import { SIMPLEGAMES,FREEGAMES } from "../mock-games";
-import { ImageService } from "./image.service";
-import { TYPES } from "../types";
-import { Message, ERROR_ID,BASE_ID } from "../../../common/communication/message";
+import { BASE_ID, ERROR_ID, Message } from "../../../common/communication/message";
 import { IGame, ISolo } from "../../../common/models/game";
-
-import { writeFileSync } from "fs";
-
+import { FREEGAMES, SIMPLEGAMES } from "../mock-games";
+import { TYPES } from "../types";
+import { ImageService } from "./image.service";
 
 @injectable()
 export class GameListService {
-
-
-    public constructor(@inject(TYPES.ImageService)private imageService: ImageService){
-        //for sprint1, load the image as string64. Will be changed for a database
-        for(let i:number=0; i < SIMPLEGAMES.length;i++)
-        {
-            SIMPLEGAMES[i].imageURL =  this.imageService.imageToString64(SIMPLEGAMES[i].imageURL);
+    public constructor(@inject(TYPES.ImageService) private imageService: ImageService) {
+        // for sprint1, load the image as string64. Will be changed for a database
+        for (const simpleGame of SIMPLEGAMES) {
+            simpleGame.imageURL = this.imageService.imageToString64(simpleGame.imageURL);
         }
-        for(let i:number=0; i < FREEGAMES.length;i++)
-        {
-            FREEGAMES[i].imageURL =  this.imageService.imageToString64(FREEGAMES[i].imageURL);
+        for (const freeGame of FREEGAMES) {
+            freeGame.imageURL = this.imageService.imageToString64(freeGame.imageURL);
         }
     }
-    
+
     public async getSimpleGames(): Promise<IGame[]> {
         return SIMPLEGAMES;
     }
@@ -35,37 +28,43 @@ export class GameListService {
         return FREEGAMES;
     }
 
-    public async addFreeGame(newGame: IGame): Promise<IGame>{
-        //Not implemented for sprint1
-        FREEGAMES.push(newGame);//mock-data for sprint1 
+    public async addFreeGame(newGame: IGame): Promise<IGame> {
+        // Not implemented for sprint1
+        FREEGAMES.push(newGame); // mock-data for sprint1
+
         return(newGame);
     }
-    public async deleteSimpleGame(gameName:string): Promise<Message> {
-        const index: number = SIMPLEGAMES.findIndex((x: IGame) => x.name ===gameName);
+    public async deleteSimpleGame(gameName: string): Promise<Message> {
+        const index: number = SIMPLEGAMES.findIndex((x: IGame) => x.name === gameName);
         if (index === -1) {
-            return {title:ERROR_ID,body:`Le jeu ${gameName} n'existe pas!`};
+            return { title: ERROR_ID, body: `Le jeu ${gameName} n'existe pas!` };
         }
         SIMPLEGAMES.splice(index, 1);
 
-        return {title:BASE_ID,body: `Le jeu ${gameName} a été supprimer`};
+        return { title: BASE_ID, body: `Le jeu ${gameName} a été supprimer` };
     }
-    public async deleteFreeGame(gameName:string): Promise<Message> {
-        const index: number = FREEGAMES.findIndex((x: IGame) => x.name ===gameName);
+    public async deleteFreeGame(gameName: string): Promise<Message> {
+        const index: number = FREEGAMES.findIndex((x: IGame) => x.name === gameName);
         if (index === -1) {
-            return {title:ERROR_ID,body:`Le jeu ${gameName} n'existe pas`};
+            return { title: ERROR_ID, body: `Le jeu ${gameName} n'existe pas` };
         }
         FREEGAMES.splice(index, 1);
 
-        return {title:BASE_ID,body:`Le jeu ${gameName} a été supprimer`};
+        return { title: BASE_ID, body: `Le jeu ${gameName} a été supprimer` };
     }
 
     public async addSimpleGame(newGame: ISolo, originalBuffer: Buffer, modifiedBuffer: Buffer): Promise<Message> {
         const name: string = newGame.name;
         const message: Message = this.imageService.getDifferentImage(name, originalBuffer, modifiedBuffer);
-        writeFileSync("./app/documents/original"+name+".bmp", originalBuffer);
-        const game: IGame = {name: message.title, imageURL: this.imageService.imageToString64("./app/documents/original"+ name +".bmp"),
-         solo: {first: 1, second: 2,third: 3}, multi:  {first: 1, second: 2,third: 3} }
+        writeFileSync("./app/documents/original" + name + ".bmp", originalBuffer);
+        const game: IGame = {
+            name: message.title,
+            imageURL: this.imageService.imageToString64("./app/documents/original" + name + ".bmp"),
+            solo: { first: 1, second: 2, third: 3 },
+            multi: { first: 1, second: 2, third: 3 },
+        };
         SIMPLEGAMES.push(game);
-        return(message);
+
+        return (message);
     }
 }
