99e0307a90c7ad0c863c0486411ab70f355387d1
Antoine Lamontagne <antoine.lamontagne@hotmail.ca>
2019-02-02 15:42:23 -0500

    fixed image controller test

--- server/app/controllers/image.controller.spec.ts
+++ server/app/controllers/image.controller.spec.ts
@@ -1,8 +1,10 @@
-import { expect } from "chai";
+import chai = require("chai");
+import spies = require("chai-spies");
 import supertest = require("supertest");
 import { Message } from "../../../common/communication/message";
 import { Application } from "../app";
 import { container } from "../inversify.config";
+import { ImageService } from "../services/image.service";
 import { TYPES } from "../types";
 
 const mockedGetDiffenrenceData: Message = {
@@ -10,27 +12,34 @@ const mockedGetDiffenrenceData: Message = {
     body: "Image with difference created",
 };
 
-const mockedImageService: Object = {
-    getDifferentImage: () => mockedGetDiffenrenceData,
-};
+// const mockedImageService: Object = {
+//     getDifferentImage: () => mockedGetDiffenrenceData,
+// };
+
+const expect: Chai.ExpectStatic = chai.expect;
+chai.use(spies);
 
 describe("Image Controller", () => {
     let app: Express.Application;
+    const sandbox: ChaiSpies.Sandbox = chai.spy.sandbox();
 
     before(() => {
         container.snapshot();
-        // container.unbind(TYPES.GameListService);
-        container.rebind(TYPES.ImageService).toConstantValue(mockedImageService);
+        const service: ImageService = container.get<ImageService>(TYPES.ImageService);
+        sandbox.on(service, "getDifferentImage", () => mockedGetDiffenrenceData);
+        sandbox.on(service, "imageToString64", () => "");
+        container.rebind(TYPES.ImageService).toConstantValue(service);
         app = container.get<Application>(TYPES.Application).app;
     });
 
     after(() => {
         container.restore();
+        sandbox.restore();
     });
 
     it("Should return image with difference created", (done: MochaDone) => {
         supertest(app)
-        .post("/imagegen")
+        .post("/api/gamelist/smple")
         .field("name", "test")
         .attach("originalImage", "./app/documents/image_test_1.bmp")
         .attach("modifiedImage", "./app/documents/image_test_2.bmp")
