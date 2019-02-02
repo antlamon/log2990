3c573c4dad25d432f3f6971dcd207b229a4cf703
Antoine Lamontagne <antoine.lamontagne@hotmail.ca>
2019-02-01 22:51:04 -0500

    removing dependency

--- server/app/services/users.service.spec.ts
+++ server/app/services/users.service.spec.ts
@@ -1,6 +1,5 @@
 import chai = require("chai");
 import spies = require("chai-spies");
-import SocketIO = require("socket.io-client");
 import { container } from "../inversify.config";
 import { TYPES } from "../types";
 import { UsersManager } from "./users.service";
@@ -9,12 +8,10 @@ const expect: Chai.ExpectStatic = chai.expect;
 chai.use(spies);
 
 describe("User service", () => {
-    const socket: SocketIOClient.Socket = SocketIO("localhost:3000");
     const service: UsersManager = container.get<UsersManager>(TYPES.UserManager);
 
     it("Adding a user should add it to the array of user", (done: MochaDone) => {
         const nbUsers: number = service.users.length;
-        socket.connect();
         expect(nbUsers).to.be.lessThan(service.users.length);
     });
 });
