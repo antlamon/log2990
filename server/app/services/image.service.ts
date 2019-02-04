0b1185a3a90fd97cd6c05210c70521acf9d53144
Antoine Lamontagne <antoine.lamontagne@hotmail.ca>
2019-02-04 18:20:53 -0500

    error handling in imagecontroller

--- server/app/services/image.service.ts
+++ server/app/services/image.service.ts
@@ -1,10 +1,10 @@
 import { readFileSync, writeFileSync } from "fs";
 import { inject, injectable } from "inversify";
 import "reflect-metadata";
-import { Message, BASE_ID, ERROR_ID } from "../../../common/communication/message";
+import { BASE_ID, ERROR_ID, Message } from "../../../common/communication/message";
+import { PATHS } from "../path";
 import { TYPES } from "../types";
 import { ConvertImage, ImageBMP, Pixel } from "./convertImage.service";
-import { PATHS } from "../path";
 
 @injectable()
 export class ImageService {
@@ -15,8 +15,8 @@ export class ImageService {
     public static readonly ENLARGE_LENGHT: number = 3;
     public static readonly ENLARGE_MAX_DIST: number = 4;
 
-    public static readonly ERROR_MESSAGE_NOT_7_ERRORS = "The given images did not have 7 differences";
-    public static readonly ERROR_MESSAGE_SIZE_NOT_COMPATIBLE = "The size of the two images are not compatibles";
+    public static readonly ERROR_MESSAGE_NOT_7_ERRORS: string = "The given images did not have 7 differences";
+    public static readonly ERROR_MESSAGE_SIZE_NOT_COMPATIBLE: string = "The size of the two images are not compatibles";
 
     public constructor(@inject(TYPES.ConvertImage) private convertImage: ConvertImage) { }
 
@@ -30,6 +30,7 @@ export class ImageService {
             }
             this.convertImage.imageBMPtoBuffer(imagesCompared, modifiedBuffer);
             writeFileSync(PATHS.DIFFERENCES_IMAGES_PATH + `${newImageName}.bmp`, modifiedBuffer);
+
             return {
                 title: BASE_ID,
                 body: newImageName,
@@ -55,41 +56,45 @@ export class ImageService {
                     callBackPixels.push([x, y]);
                     while (callBackPixels.length > 0) {
                         let hasNext: boolean = false;
-                        const current: [number, number]  = callBackPixels[callBackPixels.length - 1];
-                        for (let i: number = current[0] - 1; i <= current[0] + 1; i++ ) {
-                            for (let j: number = current[1] - 1; j <= current[1] + 1; j++ ) {
+                        const current: [number, number] = callBackPixels[callBackPixels.length - 1];
+                        for (let i: number = current[0] - 1; i <= current[0] + 1; i++) {
+                            for (let j: number = current[1] - 1; j <= current[1] + 1; j++) {
                                 if (i >= 0 && i < image.height && j >= 0 && j < image.width && !visited[i][j]) {
                                     callBackPixels.push([i, j]);
                                     hasNext = true;
-                                    visited[i][j]=true;
+                                    visited[i][j] = true;
                                 }
                             }
                         }
                         if (!hasNext) {
-                            callBackPixels.pop(); }
+                            callBackPixels.pop();
+                        }
                     }
                 }
             }
         }
+
         return diffCount;
     }
-    public createVisitedArray(pixels: Pixel[][]):boolean[][]{
-        let visited: boolean[][]=[];
-        for (let y: number = 0; y<pixels.length; ++y) {
+
+    public createVisitedArray(pixels: Pixel[][]): boolean[][] {
+        const visited: boolean[][] = [];
+        for (let y: number = 0; y < pixels.length; ++y) {
             visited.push(new Array(pixels[0].length).fill(true));
             for (let x: number = 0; x < pixels[0].length; ++x) {
-                if(this.isBlackPixel(pixels[y][x])) {
-                    visited[y][x]=false;
+                if (this.isBlackPixel(pixels[y][x])) {
+                    visited[y][x] = false;
                 }
             }
-        }  
+        }
+
         return visited;
     }
 
     public contains(array: [number, number][], item: [number, number]): boolean {
 
         for (const element of array) {
-             if ( element[0] === item[0] && element[1] === item[1]) {
+            if (element[0] === item[0] && element[1] === item[1]) {
                 return true;
             }
         }
@@ -102,7 +107,7 @@ export class ImageService {
         const pixels: Pixel[][] = [];
         const differentPixels: [number, number][] = [];
         if (image1.height !== image2.height || image1.width !== image2.width
-             || image1.height !== ImageService.IMAGE_HEIGHT || image1.width !== ImageService.IMAGE_WIDTH) {
+            || image1.height !== ImageService.IMAGE_HEIGHT || image1.width !== ImageService.IMAGE_WIDTH) {
             throw Error(ImageService.ERROR_MESSAGE_SIZE_NOT_COMPATIBLE);
         }
         for (let i: number = 0; i < image1.height; i++) {
@@ -156,7 +161,7 @@ export class ImageService {
         return (pixel.blue === 0 && pixel.green === 0 && pixel.red === 0);
     }
 
-    public imageToString64(path: string):  string {
+    public imageToString64(path: string): string {
         return "data:image/bmp;base64," + readFileSync(path).toString("base64");
     }
 }
