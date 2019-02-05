dc01a77b9e82e231b1703f20211b19230ae25d3a
Rosanne Larocque <rosanne_larocque@hotmail.com>
2019-02-05 11:47:34 -0500

    added a test for simpleGenerator RL

--- client/src/app/adminView/simple-generator/simple-generator.component.spec.ts
+++ client/src/app/adminView/simple-generator/simple-generator.component.spec.ts
@@ -1,4 +1,4 @@
-/*import { async, ComponentFixture, TestBed } from "@angular/core/testing";
+import { async, ComponentFixture, TestBed } from "@angular/core/testing";
 import { SimpleGeneratorComponent } from "./simple-generator.component";
 import { AppRoutingModule } from "src/app/app-routing.module";
 import { AdminMenuComponent } from "../admin-menu/admin-menu.component";
@@ -8,6 +8,7 @@ import { ListViewComponent } from "src/app/list-view/list-view.component";
 import { FormsModule } from "@angular/forms";
 import { HttpClientModule } from "@angular/common/http";
 import { ModalService } from "src/app/services/modal.service";
+import { By } from "@angular/platform-browser";
 
 describe("SimpleGeneratorComponent", () => {
   let component: SimpleGeneratorComponent;
@@ -33,14 +34,42 @@ describe("SimpleGeneratorComponent", () => {
   beforeEach(async () => {
     fixture = TestBed.createComponent(SimpleGeneratorComponent);
     component = fixture.componentInstance;
-    component.id="tempId";
+    component.id = "tempId";
     fixture.detectChanges();
-  
+
   });
 
   it("should create", () => {
     expect(component).toBeTruthy();
   });
 
+  it("file change event should arrive in handler", () => {
+    const input  = fixture.debugElement.query(By.css("input[type=file]")).nativeElement;
+
+    spyOn(component, "onFileChange");
+
+    input.dispatchEvent(new Event("change"));
+
+    expect(component.onFileChange).toHaveBeenCalled();
 });
-*/
+
+/*
+it('should call onFileLoaded when input is changed for required files', () => {
+
+  const fakeChangeEvent = new Event('change');
+
+  let input  = fixture.debugElement.query(By.css('input[type=file]')).nativeElement;
+
+  spyOn(component, 'onFileChange');
+
+  input.dispatchEvent(new Event('change'));
+
+  component.onFileChange(event, 'fileId' , 'labelId');
+
+  fixture.whenStable().then(() => {
+    expect(component.onFileLoaded).toHaveBeenCalledWith(fakeChangeEvent, 0, true);
+    expect(targ.files.length).toBeGreaterThan(0); //  this is always zero because i can't add to targ.files (readonly FileList)
+  });
+});*/
+
+});
