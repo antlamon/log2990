9c2a958883e5d03827140cab2fa8be5a96927307
Annie <annie1.rochette@hotmail.com>
2019-02-20 10:57:21 -0500

    Deleted unused import  - AR

--- client/src/app/gameView/game-view/game-view.component.spec.ts
+++ client/src/app/gameView/game-view/game-view.component.spec.ts
@@ -1,7 +1,7 @@
 import { async, ComponentFixture, TestBed } from "@angular/core/testing";
 import { GameViewComponent } from "./game-view.component";
 import { HttpClientModule } from "@angular/common/http";
-import { ActivatedRoute, RouterModule } from "@angular/router";
+import { RouterModule } from "@angular/router";
 
 describe("GameViewComponent", () => {
   let component: GameViewComponent;
