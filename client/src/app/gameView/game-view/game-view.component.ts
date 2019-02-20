9cb2908318648c2e74d066f9b1eae4ef8d295ba0
Annie <annie1.rochette@hotmail.com>
2019-02-20 10:21:05 -0500

    Added function to get the game's id from the URL - AR

--- client/src/app/gameView/game-view/game-view.component.ts
+++ client/src/app/gameView/game-view/game-view.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from "@angular/core";
 import { GameService } from "../../services/game.service";
 import { IGame } from "../../../../../common/models/game";
+import { ActivatedRoute } from "@angular/router";
 
 @Component({
   selector: "app-game-view",
@@ -11,15 +12,24 @@ export class GameViewComponent implements OnInit {
 
   public simpleGame: IGame;
 
-  public constructor(private gameService: GameService) { }
+  public constructor(
+    private gameService: GameService,
+    private route: ActivatedRoute
+  ) {}
 
   public ngOnInit(): void {
     this.getSimpleGame();
   }
 
+  private getId(): number {
+
+    return Number(this.route.snapshot.paramMap.get("id"));
+  }
+
   public getSimpleGame(): void {
-    this.gameService.getSimpleGame(999)
-        .then((response: IGame) => this.simpleGame = response[0] );
+    this.gameService.getSimpleGame(this.getId())
+        .then((response: IGame) => this.simpleGame = response[0] )
+        .catch(() => "getSimpleGame");
   }
 
 }
