d193e24cd306913ba42da895fb50cfafb53a14fa
Rosanne Larocque <rosanne_larocque@hotmail.com>
2019-03-08 14:25:10 -0500

    [d] deleted models generator RL

--- client/src/app/scene3D/models-generator.service.ts
+++
@@ -1,7 +0,0 @@
-import { Injectable } from "@angular/core";
-import { ShapeCreatorService } from "./scene3-d/shape-creator.service";
-
-@Injectable()
-export class ModelsGeneratorService extends ShapeCreatorService {
-
-}
