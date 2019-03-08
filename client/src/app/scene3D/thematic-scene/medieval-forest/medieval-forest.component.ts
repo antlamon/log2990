3a2e81fb301596330ccd55e9bfc8337742b4984a
Rosanne Larocque <rosanne_larocque@hotmail.com>
2019-03-08 11:57:16 -0500

    [QA] linting RL

--- client/src/app/scene3D/thematic-scene/medieval-forest/medieval-forest.component.ts
+++ client/src/app/scene3D/thematic-scene/medieval-forest/medieval-forest.component.ts
@@ -47,18 +47,14 @@ export class MedievalForestComponent extends THREE.Object3D {
   }
 
   private addCastle(): void {
+    const scale: number = 0.005;
     this.modelsLoader.load("../../assets/castle.gltf",
                            (gltf) => {
         gltf.scene.children[0].position.y = 0;
         gltf.scene.children[0].position.x = 0;
         gltf.scene.children[0].position.z = 0;
-        gltf.scene.children[0].scale.set(0.005, 0.005, 0.005);
+        gltf.scene.children[0].scale.set(scale, scale, scale);
         this.sceneRef.add(gltf.scene.children[0]);
-      },
-      // called while loading is progressing
-                           (xhr) => {
-        console.log("loading castle: " + (xhr.loaded / xhr.total * 100) + "%");
-        // could be sent as a message to user when message box is implemented
       }
     );
   }
