01c6ae6a44aea8f27c05199b576517683b0ba8bc
STEPHANE MICHAUD <stephane.michaud@polymtl.ca>
2019-03-15 15:04:28 -0400

    [T] S.M- Rename component to service in filevalidator spec

--- client/src/app/services/file-validator.service.spec.ts
+++ client/src/app/services/file-validator.service.spec.ts
@@ -1,4 +1,4 @@
-import { async, TestBed, inject } from "@angular/core/testing";
+import { async, TestBed } from "@angular/core/testing";
 import { SimpleGeneratorComponent } from "../adminView/simple-generator/simple-generator.component";
 import { AppRoutingModule } from "src/app/app-routing.module";
 import { AdminMenuComponent } from "../adminView/admin-menu/admin-menu.component";
@@ -46,42 +46,42 @@ describe("FileValidatorService", () => {
       });
   }));
 
-  const component: FileValidatorService = new FileValidatorService();
+  const service: FileValidatorService = new FileValidatorService();
 
-  it("should be created", inject([FileValidatorService], (service: FileValidatorService) => {
+  it("should be created",() => {
     expect(service).toBeTruthy();
-  }));
+  });
 
   describe("Test for the function checkBmpDimensions", () => {
     it("A bmp that is smaller than 640x480 should return false", () => {
-      expect(component.dimensionsAreValid(SMALLER_WIDTH, SMALLER_HEIGHT)).toEqual(false);
+      expect(service.dimensionsAreValid(SMALLER_WIDTH, SMALLER_HEIGHT)).toEqual(false);
     });
     it("A .bmp that is larger than 640x480 should return false", () => {
-      expect(component.dimensionsAreValid(LARGER_WIDTH, LARGER_HEIGHT)).toEqual(false);
+      expect(service.dimensionsAreValid(LARGER_WIDTH, LARGER_HEIGHT)).toEqual(false);
     });
     it("A .bmp wih null dimensions should return false", () => {
-      expect(component.dimensionsAreValid(NULL_WIDTH, NULL_HEIGHT)).toEqual(false);
+      expect(service.dimensionsAreValid(NULL_WIDTH, NULL_HEIGHT)).toEqual(false);
     });
     it("A .bmp wih 640x480 dimensions should return true", () => {
-      expect(component.dimensionsAreValid(CORRECT_WIDTH, CORRECT_HEIGHT)).toEqual(true);
+      expect(service.dimensionsAreValid(CORRECT_WIDTH, CORRECT_HEIGHT)).toEqual(true);
     });
   });
 
   describe("Test for the function onlyNumeric", () => {
     it("No letters should pass", () => {
-      expect(component.containOnlyNumeric("ALLO")).toEqual(false);
+      expect(service.containOnlyNumeric("ALLO")).toEqual(false);
     });
     it("no special caracter should pass", () => {
-      expect(component.containOnlyNumeric("!1243")).toEqual(false);
+      expect(service.containOnlyNumeric("!1243")).toEqual(false);
     });
     it("no special caracter should pass", () => {
-      expect(component.containOnlyNumeric("!12%?")).toEqual(false);
+      expect(service.containOnlyNumeric("!12%?")).toEqual(false);
     });
     it("only numbers should pass", () => {
-      expect(component.containOnlyNumeric("123")).toEqual(true);
+      expect(service.containOnlyNumeric("123")).toEqual(true);
     });
     it("@#$!@ should not pass", () => {
-      expect(component.containOnlyNumeric("@#$!@ ")).toEqual(false);
+      expect(service.containOnlyNumeric("@#$!@ ")).toEqual(false);
     });
   });
 
@@ -90,64 +90,64 @@ describe("FileValidatorService", () => {
       const delta: number = 10;
       const veryBelowMin: number = NO_MIN_OBJECTS - delta;
 
-      expect(component.isValidObjNb(veryBelowMin.toString())).toEqual(false);
+      expect(service.isValidObjNb(veryBelowMin.toString())).toEqual(false);
     });
     it("No number below the constant NO_OBJ_MIN", () => {
       const belowMin: number = NO_MIN_OBJECTS - 1;
-      expect(component.isValidObjNb(belowMin.toString())).toEqual(false);
+      expect(service.isValidObjNb(belowMin.toString())).toEqual(false);
     });
     it("should accept number equal to the constant NO_OBJ_MIN", () => {
       const min: number = NO_MIN_OBJECTS;
-      expect(component.isValidObjNb(min.toString())).toEqual(true);
+      expect(service.isValidObjNb(min.toString())).toEqual(true);
     });
     it("No number bigger than the constant NO_OBJ_MAX", () => {
       const delta: number = 10;
       const veryMAX: number = NO_MAX_OBJECTS + delta;
 
-      expect(component.isValidObjNb(veryMAX.toString())).toEqual(false);
+      expect(service.isValidObjNb(veryMAX.toString())).toEqual(false);
     });
     it("No number below the constant NO_OBJ_MIN", () => {
       const tooMuch: number = NO_MAX_OBJECTS + 1;
-      expect(component.isValidObjNb(tooMuch.toString())).toEqual(false);
+      expect(service.isValidObjNb(tooMuch.toString())).toEqual(false);
     });
     it("should accept number equal to the constant NO_OBJ_MIN", () => {
       const max: number = NO_MAX_OBJECTS;
-      expect(component.isValidObjNb(max.toString())).toEqual(true);
+      expect(service.isValidObjNb(max.toString())).toEqual(true);
     });
   });
 
   describe("Test for the name of the game", () => {
     it("A name longer than 15 characters should return false", () => {
-      expect(component.isValidGameName("abcdefghijklmnopqrstuvwxyz")).toEqual(false);
+      expect(service.isValidGameName("abcdefghijklmnopqrstuvwxyz")).toEqual(false);
     });
     it("A name shorter than 3 characters should return false", () => {
-      expect(component.isValidGameName("az")).toEqual(false);
+      expect(service.isValidGameName("az")).toEqual(false);
     });
     it("A name that contains between 3 and 15 characters should return true", () => {
-      expect(component.isValidGameName("nomdujeu")).toEqual(true);
+      expect(service.isValidGameName("nomdujeu")).toEqual(true);
     });
     it("A name that does not only contains alphanumeric characters should return false", () => {
-      expect(component.containOnlyAlphaNumeric("@#$ans")).toEqual(false);
+      expect(service.containOnlyAlphaNumeric("@#$ans")).toEqual(false);
     });
     it("A name that only contains alphanumeric characters should return true", () => {
-      expect(component.containOnlyAlphaNumeric("dksjhfad24324")).toEqual(true);
+      expect(service.containOnlyAlphaNumeric("dksjhfad24324")).toEqual(true);
     });
     it("A NULL name should return false", () => {
-      expect(component.isValidGameName("")).toEqual(false);
+      expect(service.isValidGameName("")).toEqual(false);
     });
   });
   describe("Test for the name of the username", () => {
     it("A name longer than 10 characters should return false", () => {
-      expect(component.isValidName("abcdefghijklmnopqrstuvwxyz")).toEqual(false);
+      expect(service.isValidName("abcdefghijklmnopqrstuvwxyz")).toEqual(false);
     });
     it("A name shorter than 3 characters should return false", () => {
-      expect(component.isValidName("az")).toEqual(false);
+      expect(service.isValidName("az")).toEqual(false);
     });
     it("A name that contains between 3 and 15 characters should return true", () => {
-      expect(component.isValidName("nomdujeu")).toEqual(true);
+      expect(service.isValidName("nomdujeu")).toEqual(true);
     });
     it("A NULL name should return false", () => {
-      expect(component.isValidName("")).toEqual(false);
+      expect(service.isValidName("")).toEqual(false);
     });
   });
 });
