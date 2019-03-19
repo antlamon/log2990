ce681087c09eddf9bd3a9cbef9f4ff5419c63841
Jonathan <jonathan.helias@polymtl.ca>
2019-03-19 09:44:27 -0400

    [Dev] JH adding position handler (needs reformat)

--- client/src/app/scene3D/scene3-d/render.service.ts
+++ client/src/app/scene3D/scene3-d/render.service.ts
@@ -188,25 +188,32 @@ export class RenderService {
     }
   }
 
-  private identifyDiff(event: MouseEvent): void {
-    console.log(this.containerOriginal.offsetLeft)
-    console.log(this.containerOriginal.offsetTop)
-    console.log(event.clientX)
-    console.log(event.clientY)
+  private identifyDiff(event: MouseEvent): THREE.Vector3 {
+
+    const URL: string = "api/identification3D";
+    const MULTI: number = 2;
+    console.log(this.containerOriginal.offsetLeft);
+    console.log(this.containerOriginal.offsetTop);
+    console.log(event.clientX);
+    console.log(event.clientY);
     if ( event.clientX < this.containerModif.offsetLeft) {
-      this.mouse.x = ((event.clientX - this.containerOriginal.offsetLeft) / this.containerOriginal.offsetWidth) * 2 - 1;
-      this.mouse.y = -((event.clientY - this.containerOriginal.offsetTop) / this.containerOriginal.offsetHeight) * 2 + 1;
+      this.mouse.x = ((event.clientX - this.containerOriginal.offsetLeft) / this.containerOriginal.offsetWidth) * MULTI - 1;
+      this.mouse.y = -((event.clientY - this.containerOriginal.offsetTop) / this.containerOriginal.offsetHeight) * MULTI + 1;
     } else {
-      this.mouse.x = ((event.clientX - this.containerModif.offsetLeft) / this.containerModif.offsetWidth) * 2 - 1;
-      this.mouse.y = -((event.clientY - this.containerModif.offsetTop) / this.containerModif.offsetHeight) * 2 + 1;
+      this.mouse.x = ((event.clientX - this.containerModif.offsetLeft) / this.containerModif.offsetWidth) * MULTI - 1;
+      this.mouse.y = -((event.clientY - this.containerModif.offsetTop) / this.containerModif.offsetHeight) * MULTI + 1;
     }
 
     this.raycaster.setFromCamera( this.mouse, this.camera );
     const intersects: THREE.Intersection[] = this.raycaster.intersectObjects( this.sceneOriginal.children);
-    console.log(this.mouse);
     if (intersects.length > 0) {
-      //const obj: THREE.Object3D = intersects[0].object;
       this.sceneOriginal.background = new THREE.Color(0x000000);
+      const obj: THREE.Object3D = intersects[0].object;
+
+      return obj.position;
+    } else {
+
+      return new THREE.Vector3(0, 0, 0);
     }
   }
 }
