ebde2e86a16a0e63e749e4243347d609428d2281
Jonathan <jonathan.helias@polymtl.ca>
2019-03-21 18:17:41 -0400

    [Dev] JH structure for identificationservice3D

--- server/app/services/identification3D.service.ts
+++ server/app/services/identification3D.service.ts
@@ -1,11 +1,18 @@
 import { IGame3D } from "../../../common/models/game3D";
 
 export class Identification3DService {
- 
-    public constructor(private game: IGame3D) { }
 
+    public constructor(private game: IGame3D) {
+    }
+    //TODO: implement with correct structure
     public getDifference(objName: string): string {
 
-        return objName;
+        for (const obj of this.game.originalScene.objects) {
+            if (obj.type === objName) {
+                return objName;
+            }
+        }
+
+        return "TODO";
     }
 }
