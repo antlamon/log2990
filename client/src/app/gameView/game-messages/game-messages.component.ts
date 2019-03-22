1912a4bc2622c924ed5ce1a49cc80e8faa58fc4f
Annie <annie1.rochette@hotmail.com>
2019-03-22 11:09:57 -0400

    [D] Added function getMessages()

--- client/src/app/gameView/game-messages/game-messages.component.ts
+++ client/src/app/gameView/game-messages/game-messages.component.ts
@@ -1,4 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import { SocketService } from 'src/app/services/socket.service';
+import { SocketsEvents } from '../../../../../common/communication/socketsEvents';
+import { MessageService } from "../../services/message.service";
+import { IGameMessage } from "../../../../../common/models/simpleGameMessage";
 
 @Component({
   selector: 'app-game-messages',
@@ -7,9 +11,20 @@ import { Component, OnInit } from '@angular/core';
 })
 export class GameMessagesComponent implements OnInit {
 
-  constructor() { }
+  public gameMessages: IGameMessage[];
 
-  ngOnInit() {
+  public constructor(private socket: SocketService, private gameMessageService: MessageService) {
+    this.socket.addEvent(SocketsEvents.NEW_GAME_MESSAGE, this.getMessages.bind(this));
+  }
+
+  public ngOnInit(): void {
+    this.getMessages();
+  }
+
+  public getMessages(): void {
+    this.gameMessageService.getMessages()
+      .subscribe((response: IGameMessage[]) => {this.gameMessages = response; });
+    (console as Console).log(this.gameMessages);
   }
 
 }
