c8f715ec526e2663fa6baf21f364c2d9da5efd53
Annie <annie1.rochette@hotmail.com>
2019-03-22 11:08:40 -0400

    [D] Added message interface

---
+++ common/models/simpleGameMessage.ts
@@ -0,0 +1,20 @@
+
+export interface IGameMessage{
+    eventType: string;
+    time: string;
+    username: string;
+    data: string;
+}
+
+export interface IMessageForm{
+    eventType: string;
+    username: string;
+}
+
+export interface BestTimeMessage {
+    msg: IGameMessage;
+    position: number;
+    gameName: string;
+    nbPlayers: string;
+}
+
