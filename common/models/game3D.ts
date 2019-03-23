f7d2d611fb4a70b580932d474d8b44d0be50b90e
Rosanne Larocque <rosanne_larocque@hotmail.com>
2019-03-22 21:43:51 -0400

    [dev] added mock THEMEd for easier testing RL

--- common/models/game3D.ts
+++ common/models/game3D.ts
@@ -1,6 +1,15 @@
 import { IObjet3D } from "./objet3D";
 import { IScore } from "./top3";
 
+export const ADD_TYPE: string = "ADD";
+export const DELETE_TYPE: string = "DELETE";
+export const MODIFICATION_TYPE: string = "MODIF";
+export const GEOMETRIC_TYPE_NAME: string = "geometric";
+export const THEMATIC_TYPE_NAME: string = "thematic";
+export const NO_MIN_OBJECTS: number = 10;
+export const NO_MAX_OBJECTS: number = 200;
+export const MODIFIED: string = "mod";
+export const ORIGINAL: string = "original";
 export interface IGame3D {
 
     name: string;
@@ -17,12 +26,111 @@ export interface IDifference{
     object?: IObjet3D;
     name: string;
 }
-export const ADD_TYPE: string = "ADD";
-export const DELETE_TYPE: string = "DELETE";
-export const MODIFICATION_TYPE: string = "MODIF";
-export const GEOMETRIC_TYPE_NAME: string = "geometric";
-export const THEMATIC_TYPE_NAME: string = "thematic";
-export const NO_MIN_OBJECTS: number = 10;
-export const NO_MAX_OBJECTS: number = 200;
-export const MODIFIED: string = "mod";
-export const ORIGINAL: string = "original";
+export const MOCK_THEMED_GAME: IGame3D  = {
+    name: "MOCKGAME",
+    id: "0",
+    originalScene: 
+     [
+            {
+                type: "knight",
+                name: "1",
+                position: { x: 20, y: 0, z: -20},
+                size: 1,
+                rotation: { x: 0, y: 0, z: 0},
+            },
+            {
+                type: "rock",
+                name: "2",
+                position: { x: 10, y: 0, z: -15},
+                size: 1,
+                rotation: { x: 0, y: 0, z: 0},
+            },
+            {
+                type: "dragon",
+                name: "3",
+                position: { x: 15, y: 0, z: -15},
+                size: 1,
+                rotation: { x: 0, y: 0, z: 0},
+            },
+            {
+                type: "balista",
+                name: "4",
+                position: { x: 10, y: 0, z: -10},
+                size: 1,
+                rotation: { x: 0, y: 0, z: 0},
+            },
+            {
+                type: "balista",
+                name: "5",
+                position: { x: 10, y: 0, z: -10},
+                size: 1,
+                rotation: { x: 0, y: 0, z: 0},
+            },            {
+                type: "balista",
+                name: "6",
+                position: { x: 10, y: 0, z: -10},
+                size: 1,
+                rotation: { x: 0, y: 0, z: 0},
+            },
+            {
+                type: "balista",
+                name: "7",
+                position: { x: 10, y: 0, z: -10},
+                size: 1,
+                rotation: { x: 0, y: 0, z: 0},
+            },            {
+                type: "balista",
+                name: "8",
+                position: { x: 10, y: 0, z: -10},
+                size: 1,
+                rotation: { x: 0, y: 0, z: 0},
+            },
+            {
+                type: "balista",
+                name: "9",
+                position: { x: 10, y: 0, z: -10},
+                size: 1,
+                rotation: { x: 0, y: 0, z: 0},
+            },
+        
+        ],
+    solo: [],
+    multi: [],
+    differences: [
+        {
+            type: ADD_TYPE,
+            object:             {
+                type: "dragon",
+                name: "9",
+                position: { x: 10, y: 0, z: -10},
+                size: 1,
+                rotation: { x: 0, y: 0, z: 0},
+            },
+            name: "",
+        },
+        {
+            type: ADD_TYPE,
+            object:             {
+                type: "rock",
+                name: "9",
+                position: { x: -10, y: 0, z: -10},
+                size: 1,
+                rotation: { x: 0, y: 0, z: 0},
+            },
+            name: "",
+        },
+        {
+            type: ADD_TYPE,
+            object:             {
+                type: "dragon",
+                name: "",
+                position: { x: -10, y: 0, z: -10},
+                size: 1,
+                rotation: { x: 0, y: 0, z: 0},
+            },
+            name: "",
+        },
+    ],
+    isThematic: true,
+    backColor: 0,
+};
