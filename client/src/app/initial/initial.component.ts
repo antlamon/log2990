2d873d4a2105aac20e1a176b2f7ba684f9ff8ea4
STEPHANE MICHAUD <stephane.michaud@polymtl.ca>
2019-04-10 09:47:11 -0400

    [D] SM JH - Added key enter for connect

--- client/src/app/initial/initial.component.ts
+++ client/src/app/initial/initial.component.ts
@@ -1,4 +1,4 @@
-import { Component } from "@angular/core";
+import { Component} from "@angular/core";
 import { Router } from "@angular/router";
 import { BASE_ID, ERROR_ID, Message } from "../../../../common/communication/message";
 import { FileValidatorService } from "../services/file-validator.service";
